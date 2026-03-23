927c0bb50e64847dc6f71df296a4a3c61baaa22b
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-23 10:12:17 +0000

    Day 12

---
+++ chapter2/OperatorsLesson.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class OperatorsLesson {
+    public static void main(String[] args) {
+     // shift ops
+        System.out.println(2<<2);
+        System.out.println(15>>1);
+        //0 0 1 0
+        //8 4 2 1
+
+        //01 1 1 1 
+        int p = 3;
+        System.out.println(++p+ p++);
+        System.out.println(!false);
+}
+}
