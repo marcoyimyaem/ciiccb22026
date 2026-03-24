aae6f742aa5929411aed49d152658a2e31a6c879
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-24 08:02:14 +0000

    Day 13 end

--- chapter2/EqualityOperatorLesson.java
+++ chapter2/EqualityOperatorLesson.java
@@ -16,3 +16,18 @@ String name;
    Person(String name){
    this.name=name;}
 }
+
+class EqualityOperator {
+    public static void main(String[] args) {
+        File x = new File();
+        File y = new File();
+        File z = x;
+        System.out.println(x == y);
+        System.out.println(x == z);
+
+    }
+}
+
+class File{
+
+}
--- chapter2/IfStatementsLesson.java
+++ chapter2/IfStatementsLesson.java
@@ -5,7 +5,51 @@ public class IfStatementsLesson {
         return false;
     } 
     public static void main(String[] args) {
-        int hourOfDay = 20;
+         int hourOfDay=22;
+        boolean good = false;
+        if(hourOfDay<11)
+            System.out.println("Good Morning");
+        System.out.println("Kumain kana ba?");
+
+        //if else
+        if(hourOfDay<11)
+            System.out.println("Good Morning");
+        else
+            System.out.println("Good Afternoon");
+        System.out.println("Good Afternoon");
+
+        //if, else if, else
+        if(hourOfDay>=18)
+            System.out.println("Good Eve");
+        else if(hourOfDay>=12)
+            System.out.println("Good Afternoon");
+        else
+            System.out.println("Good Morning");
+        // (boolean expression) ? T : F;
+        System.out.println((hourOfDay>=18)?"Good Eve":(hourOfDay>=12)?"Good Afternoon":"Good Morning");
+        int dayOfWeek =9;
+        String yes = "Hello";
+        final String no = "Hell noh";
+        switch(dayOfWeek){
+            
+            case 1: System.out.println("Monday");
+                    break;
+            case 2: System.out.println("Tuesday");
+                    break;
+            case 3: System.out.println("Wednesday");
+                    break;
+            default:
+                    System.out.println("Invalid Day of Week");
+            case 4: System.out.println("Thusday");
+                    break;
+            case 5: System.out.println("Friday");
+                    break;
+            case 6: 
+            case 7: System.out.println("Weekends");
+                    break;
+            
+        }
+         hourOfDay = 20;
         boolean isMorning = true;
         if(hourOfDay<11 )
             System.out.println("Good Morning1");
