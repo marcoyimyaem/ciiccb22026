b2915064d25d5b2e4e78cb54674dd4c26f06fba8
marcoyimyaem <marcoyimyaem@gmail.com>
2026-03-31 08:01:37 +0000

    Day 19

--- Test/Task1.java
+++ Test/Task1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Task1 {
 public static void main(String[] args) {
-    System.out.println("desktop edit");
+    Scanner opt = new Scanner(System.in);
+    boolean cont = true;
+    while(cont){
+
+        //program here
+        System.out.println("Do you want to continue?");
+        String select = opt.nextLine();
+        cont = (select.toLowerCase().equals("y") )? true:false;
+
+    }
+    System.out.println("end of program");
+    //create a program that will generate a random characters based on user input
+
+    //input: 10
+    //output: (}h3ikHjfG
 }
 }
--- Test/Tester.java
+++ Test/Tester.java
@@ -11,11 +11,16 @@ public class Tester {
         String a,b;
         Tester t1 = new Tester();
         Toy toy1 = new Toy();
+        Toy toy2 = new Toy();
         a=t1.name;
         b=toy1.name;
         System.out.println((a.toLowerCase().startsWith("gun")?" match found":"not matching"));
         System.out.println(toProperString("SCHOOLOFPROG"));
         System.out.println(toProperString("progprince"));
+        System.out.println(toy1);
+        System.out.println(toy2);
+        
+        
         
     }
 }
